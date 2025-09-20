1d9776f7250ddc6164e207906d95ff2152a69b2c
sergey <sbitza@gmail.com>
2025-09-20 22:35:49 +0300

    changed debug mode

--- dentalstore/dentalstore/settings.py
+++ dentalstore/dentalstore/settings.py
@@ -28,8 +28,8 @@ FAVORITES_SESSION_ID = 'favorites'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-DEBUG = 'False'
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+# DEBUG = 'False'
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
